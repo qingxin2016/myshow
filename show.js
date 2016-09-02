e14c4996659e7612df8f322002f4c3e79980f9cd
qingxin2016 <qingxin2016@gmail.com>
2016-09-02 16:24:33 +0900

    Add files via upload

---
+++ show.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function ($) {
+    var showpop = false;
+    $.fn.myshow = function (options) {
+
+        var config = $.extend({
+            orignal: '.list',
+            target: '.wrapper',
+            close: '.btn-danger'
+        }, options);
+
+        var setting = $.extend({}, config, options);
+
+        
+
+        var action = {
+            show: function (id) {
+                 console.log('start:'+showpop);
+                if (showpop) {
+                    return;
+                } else {
+
+                    $('#' + id).find(setting.target).slideDown();
+                    showpop = true;
+                }
+                console.log('end:'+showpop);
+
+            },
+            close: function (id) {
+
+                $('#' + id).find(setting.target).slideUp();
+                showpop = false;
+            }
+        };
+
+        return this.each(function () {
+
+            $(this).click(function () {
+                var id = 'id' + parseInt(Math.random() * 100000);
+                $(this).parents(setting.orignal).attr('id', id);
+                action.show(id);
+
+            });
+
+            $(this).parents(setting.orignal).find(setting.close).click(function () {
+                var id = $(this).parents(setting.orignal).attr('id');
+                action.close(id);
+            });
+
+        });
+    };
+
+}(jQuery));
+
+$('.btn-warning').myshow();
+$('.btn-info').myshow();
+$('.btn-success').myshow({
+    orignal: '.wwww',
+    target: '.wrapper02'
+});
